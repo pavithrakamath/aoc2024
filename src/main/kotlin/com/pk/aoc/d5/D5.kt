cd9d7354095cad44e54aae6f93fba85c74cea008
tp00214351 <pavithra.kamath@thoughtworks.com>
2024-12-05 13:57:14 +0530

    AOC-1|PK|Commits for day 5 - 2nd Part

--- src/main/kotlin/com/pk/aoc/d5/D5.kt
+++ src/main/kotlin/com/pk/aoc/d5/D5.kt
@@ -6,40 +6,57 @@ class D5 {
 }
 
 fun main() {
-    val rules = D5::class.java.getResource("/D5Rules.txt").readText().split("\n")
-    val ruleMap = mutableMapOf<Int, List<Int>>()
-    rules.forEach {
-        val parts = it.split("|")
-        ruleMap[parts[1].toInt()] = ruleMap.getOrDefault(parts[1].toInt(), emptyList()) + parts[0].toInt()
-    }
-    //read D5Input.txt as a List<List<Int>> separated by comma
-    val input = D5::class.java.getResource("/D5Input.txt").readText().split("\n").map {
-        it.split(",").map { it.toInt() }
-    }
-    var count = 0
-    var orderedPages = listOf<Int>()
-    input.forEach {
+    val rules = D5::class.java.getResource("/D5Rules.txt").readText().lineSequence()
+    val ruleMap = rules.map { it.split("|").let { parts -> parts[1].toInt() to parts[0].toInt() } }
+        .groupBy({ it.first }, { it.second })
+
+    val input =
+        D5::class.java.getResource("/D5Input.txt").readText().lineSequence().map { it.split(",").map(String::toInt) }
+
+    val unordered = mutableListOf<List<Int>>()
+    input.sumOf {
         val midPage = findMidIfOrdered(it, ruleMap)
-        if (midPage != 0) {
-            orderedPages += midPage
+        if (midPage == 0) {
+            unordered.add(it)
         }
-        count += midPage
+        midPage
+    }.let { println("Total of mid pages of ordered = $it") }
+    println("Total of mid pages for reordered = ${unordered.sumOf { reorderPages(it, ruleMap) }}")
+
+}
+
+fun reorderPages(orderedPages: List<Int>, rules: Map<Int, List<Int>>): Int {
+    val ordered = orderedPages.toMutableList()
+    var i = 0
+    while (i < ordered.size) {
+        //rule holds all pages that should come before the current page
+        val rule = rules[ordered[i]]
+        if (rule != null) {
+            val indexes = rule.mapNotNull {
+                // for each page that should come before the current page, find the index of the page in the ordered list
+                ordered.indexOf(it).takeIf { idx -> idx > i }
+            }
+            if (indexes.isNotEmpty()) {
+                val temp = ordered[i]
+                val mid = indexes[indexes.size / 2]
+                ordered[i] = ordered[mid]
+                ordered[mid] = temp
+                i = 0
+                continue
+            }
+        }
+        i++
     }
-    println("Total count = $count")
-
-
+    return ordered[ordered.size / 2]
 }
 
 fun findMidIfOrdered(pages: List<Int>, rules: Map<Int, List<Int>>): Int {
     for (i in pages.indices) {
         val rule = rules[pages[i]]
-        if (rule != null) {
-            if (rule.any { pages.indexOf(it) > i }) {
-                return 0
-            }
+        if (rule != null && rule.any { pages.indexOf(it) > i }) {
+            return 0
         }
     }
-    println("this page is ordered $pages")
     return pages[pages.size / 2]
 }
 
